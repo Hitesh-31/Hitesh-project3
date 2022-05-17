2b76008492929e0aedfd4d1aa23fd313e49c7fb6
Hitesh-31 <95993585+Hitesh-31@users.noreply.github.com>
2022-05-17 17:30:20 +0530

    Update allMiddleware.js

--- src/middleware/allMiddleware.js
+++ src/middleware/allMiddleware.js
@@ -77,40 +77,4 @@ const authorization2 =  function(req,res,next){
 }
 
 module.exports = {authentication,authorization1,authorization2}
-        let id = req.userId;
-        if(!isValidObjectId(bookId)){
-            return res.status(400).send({ status: false, message: "Please enter valid bookId" })
-         }
-         let book = await bookModel.findOne({_id:bookId, isDeleted:false});
-        if(!book){
-            return res.status(404).send({ status: false, message: "No such book" }) 
-        }
-        if(id != book.userId){
-            return res.status(403).send({status: false , message : "Not authorized..!" });
-        }
-        next();
-    }
-    catch (error) {
-        return res.status(500).send({ status: false, message: error.message });
-    }
-}
-
-const authorization2 = async function(req,res,next){
-    try{
-        let createId = req.body.userId;
-        let id = req.userId;
-        if(!isValidObjectId(createId)){
-            return res.status(400).send({ status: false, message: "Please enter valid userId" })
-         }
-        if(id != createId){
-            return res.status(403).send({status: false , message : "Not authorized..!" });
-        }
-        next();
-    }
-    catch (error) {
-        return res.status(500).send({ status: false, message: error.message });
-    }
-}
-
-module.exports = {authentication,authorization1,authorization2}
 
