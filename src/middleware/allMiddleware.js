531bb666f018e48e854cd4d016f34de4ee30be70
Hitesh-31 <95993585+Hitesh-31@users.noreply.github.com>
2022-05-17 17:28:26 +0530

    Update allMiddleware.js

--- src/middleware/allMiddleware.js
+++ src/middleware/allMiddleware.js
@@ -51,6 +51,50 @@ const authorization1 = async function(req,res,next){
     }
 }
 
+const authorization2 =  function(req,res,next){
+    try{
+        let createId = req.body.userId;
+        let id = req.userId;
+        const isValidRequestBody = function (requestBody) {
+            return Object.keys(requestBody).length > 0
+        }
+        if (!isValidRequestBody(req.body)){
+        return res.status(400).send({ status: false, message: "Please fill book details" })
+        }
+
+        if(!isValidObjectId(createId)){
+            return res.status(400).send({ status: false, message: "Please enter valid userId" })
+         }
+         
+        if(id != createId){
+            return res.status(403).send({status: false , message : "Not authorized..!" });
+        }
+        next();
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
+    }
+}
+
+module.exports = {authentication,authorization1,authorization2}
+        let id = req.userId;
+        if(!isValidObjectId(bookId)){
+            return res.status(400).send({ status: false, message: "Please enter valid bookId" })
+         }
+         let book = await bookModel.findOne({_id:bookId, isDeleted:false});
+        if(!book){
+            return res.status(404).send({ status: false, message: "No such book" }) 
+        }
+        if(id != book.userId){
+            return res.status(403).send({status: false , message : "Not authorized..!" });
+        }
+        next();
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
+    }
+}
+
 const authorization2 = async function(req,res,next){
     try{
         let createId = req.body.userId;
